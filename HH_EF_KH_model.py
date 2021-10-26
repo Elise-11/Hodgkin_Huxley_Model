ca5692c9c0d5e81a9862d459b85aff5b3ed69a53
Elise-11 <elise.fabre@outlook.com>
2021-10-26 11:52:35 +0200

    test simulation

--- HH_EF_KH_model.py
+++ HH_EF_KH_model.py
@@ -227,13 +227,14 @@ sim = Simulation(model)
 sim.runSimulation(membraneVoltage=target_voltage, stepMs=0.01)
 plt.figure(figsize=(10, 8))
 
+#membrane potential 
 ax1 = plt.subplot(311)
 ax1.plot(sim.times, sim.Vm - 70, color='b')
 ax1.set_ylabel("Membrane Potential (mV)")
 ax1.set_xlabel(" Time (ms)")
 ax1.set_title("Hodgkin-Huxley Model", fontSize=15)
 
-
+#Currents
 ax2 = plt.subplot(312, sharex=ax1)
 ax2.plot(sim.times, sim.INa, label='INa', color='cyan')
 ax2.plot(sim.times, sim.IK, label='IK', color = 'red')
@@ -241,6 +242,16 @@ ax2.plot(sim.times, sim.ILeak, label='ILeak', color = 'pink')
 ax2.set_ylabel("Current (µA/cm²)")
 ax2.set_xlabel(" Time (ms)")
 ax2.legend()
+
+#gating variable states
+ax3 = plt.subplot(313, sharex=ax1)
+ax3.plot(sim.times, sim.h_state, label='h', color="darkcyan")
+ax3.plot(sim.times, sim.m_state, label='m', color="dodgerblue")
+ax3.plot(sim.times, sim.n_state, label='n', color = "red")
+ax3.set_xlabel(" Time (ms)")
+ax3.set_ylabel("Gating variable activation")
+ax3.legend()
+
 
 plt.tight_layout()
 plt.show()
